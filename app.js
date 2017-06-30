740cf953defb71616074146cb80019eac98ae522
mjjoo0607 <minjoojoo67@gmail.com>
2017-06-30 10:16:21 -0500

    1

--- app.js
+++ app.js
@@ -42,6 +42,6 @@ app.post('/experiment-data', function(request, response){
 })
 
 
-var server = app.listen(process.env.PORT, function(){
+var server = app.listen(process.env.PORT|| 5000, function(){
     console.log("Listening on port %d", server.address().port);
 });
